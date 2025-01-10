2d0035ea2ed23f2cde14d74052088ec7b04a0fbe
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-10 10:40:50 +0200

    add @vercel/analytics package and integrate Analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import VNavbar from "./components/VNavbar";
 import Configurator from "./views/Configurator";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <Box>
       <VNavbar />
       <Configurator />
+      <Analytics />
     </Box>
   );
 }
