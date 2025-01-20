7331c7a65dc74c855aa661f14795bb2804b2dd5b
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-20 20:51:58 +0200

    refactor main.jsx; remove ThemeProvider wrapper from rendering

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,14 @@ import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ThemeProvider from "./theme/index.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <App />
-        </Suspense>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
   </StrictMode>
 );
