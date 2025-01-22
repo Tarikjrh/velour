aecccf8f5245cbb606894013bb919f6c9794e365
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-22 21:41:16 +0200

    fix: update category titles for better clarity and accuracy

--- src/views/LandingPage/utils/categories_data.js
+++ src/views/LandingPage/utils/categories_data.js
@@ -1,10 +1,10 @@
 const categories_data = [
-  { img: "/categories/corporate.png", title: "Corporate/Office" },
+  { img: "/categories/corporate.png", title: "Corporate/Hospitality" },
   { img: "/categories/healthcare.png", title: "Healthcare" },
   { img: "/categories/education.png", title: "Education" },
-  { img: "/categories/industrial.png", title: "Industrial/Work wear" },
-  { img: "/categories/transport.png", title: "Transport/Logistics" },
-  { img: "/categories/retail.png", title: "Retail/Customer Service" },
+  { img: "/categories/industrial.png", title: "Industrial/Workwear" },
+  { img: "/categories/transport.png", title: "Essentials" },
+  { img: "/categories/retail.png", title: "Accessories" },
 ];
 
 export default categories_data;
