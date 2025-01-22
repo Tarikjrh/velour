1cecd9bd404f6a9474327125502a7dd43ad62aee
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-22 16:27:18 +0200

    style: increase minHeight of OtherContact component and adjust padding

--- src/views/ContactUs/components/OtherContact.jsx
+++ src/views/ContactUs/components/OtherContact.jsx
@@ -6,20 +6,15 @@ export default function OtherContact() {
   return (
     <Box
       sx={{
-        minHeight: "70vh",
+        minHeight: "80vh",
         background: `linear-gradient(175.81deg, rgba(44, 120, 49, 0.063) 27.37%, rgba(46, 127, 81, 0.112) 96.85%)`,
       }}
     >
-      <Stack
-        spacing={2}
-        alignItems="center"
-        direction="row"
-        sx={{ position: "relative" }}
-      >
-        <Box sx={{ position: "absolute", top: "-100%" }}>
+      <Stack spacing={2} alignItems="center" direction="row">
+        <Box>
           <Image src="contact_phone.png" />
         </Box>
-        <Box sx={{ pl: 20 }}>
+        <Box sx={{ pl: 10 }}>
           <Typography variant="h4">Other Ways to Reach Us</Typography>
           <Typography variant="h4">
             123 Velour Lane, Beirut, Lebanon. Phone: +961 123 4567 Email:
