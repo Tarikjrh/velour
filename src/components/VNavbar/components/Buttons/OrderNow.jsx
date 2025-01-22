dc0b5ab6a1862b79d4f03f8270d073922c80b019
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-22 16:25:28 +0200

    feat: add shopping bag icon to Order Now button

--- src/components/VNavbar/components/Buttons/OrderNow.jsx
+++ src/components/VNavbar/components/Buttons/OrderNow.jsx
@@ -1,8 +1,13 @@
 import { Button } from "@mui/material";
+import Iconify from "../../../iconify";
 
 export default function OrderNow(props) {
   return (
-    <Button variant="contained" {...props}>
+    <Button
+      variant="contained"
+      {...props}
+      startIcon={<Iconify icon="heroicons:shopping-bag" />}
+    >
       Order Now
     </Button>
   );
