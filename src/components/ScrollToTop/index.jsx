e04f20ecca9156c4662bf0d6c049ee8d7e09fa0b
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-28 16:35:31 +0200

    feat: add ScrollToTop component for improved user navigation

---
+++ src/components/ScrollToTop/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Fab, Zoom } from "@mui/material";
+// import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { useScrollTrigger, Box } from "@mui/material";
+import Iconify from "../iconify";
+
+const ScrollToTop = ({ window }) => {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100, // Show the button when scrolled 100px
+  });
+
+  const handleClick = () => {
+    // Ensure the scrollable container is targeted
+    const anchor = document.querySelector("html");
+    if (anchor) {
+      anchor.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+  return (
+    <Zoom in={trigger}>
+      <Box
+        role="presentation"
+        sx={{
+          position: "fixed",
+          bottom: 16,
+          right: 16,
+          zIndex: 1000,
+        }}
+      >
+        <Fab
+          color="primary"
+          size="small"
+          onClick={handleClick}
+          aria-label="Scroll to top"
+        >
+          <Iconify icon="uis:angle-up" />
+          {/* <KeyboardArrowUpIcon /> */}
+        </Fab>
+      </Box>
+    </Zoom>
+  );
+};
+
+export default ScrollToTop;
--- src/layout/SimpleLayout.jsx
+++ src/layout/SimpleLayout.jsx
@@ -3,6 +3,8 @@ import VNavBar from "../components/VNavbar";
 import Footer from "../components/Footer";
 import SplashScreen from "../components/SplashScreen";
 import { Box } from "@mui/material";
+import ScrollToTop from "../components/ScrollToTop";
+import PropTypes from "prop-types";
 
 export default function SimpleLayout({ children, noPaddingTop }) {
   return (
@@ -12,7 +14,13 @@ export default function SimpleLayout({ children, noPaddingTop }) {
       <Suspense fallback={<SplashScreen />}>
         <Box sx={{ pt: noPaddingTop ? 15 : 0 }}>{children}</Box>
       </Suspense>
+      <ScrollToTop />
       <Footer />
     </>
   );
 }
+
+SimpleLayout.propTypes = {
+  children: PropTypes.node,
+  noPaddingTop: PropTypes.bool,
+};
