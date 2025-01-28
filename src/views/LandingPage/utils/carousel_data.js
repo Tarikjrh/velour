1ced901315828a6ff563383a11f2e42840d76b1e
Tarikjrh <tarik.jarrah@hotmail.com>
2025-01-28 16:35:25 +0200

    feat: update carousel cover images for consistency

--- src/views/LandingPage/utils/carousel_data.js
+++ src/views/LandingPage/utils/carousel_data.js
@@ -7,12 +7,12 @@ const carousel_data = [
   {
     id: "e99f09a7-dd88-49d5-b1c8-1daf80c2d7b2",
     title: "The Ultimate Guide to Productivity Hacks",
-    coverUrl: "/landing-car-2.png",
+    coverUrl: "/landing-car-1.png",
   },
   {
     id: "e99f09a7-dd88-49d5-b1c8-1daf80c2d7b3",
     title: "Exploring the Hidden Gems of [Destination]",
-    coverUrl: "/landing-car-3.png",
+    coverUrl: "/landing-car-1.png",
     description:
       "Rerum eius velit dolores. Explicabo ad nemo quibusdam. Voluptatem eum suscipit et ipsum et consequatur aperiam quia. Rerum nulla sequi recusandae illum velit quia quas. Et error laborum maiores cupiditate occaecati.",
   },
