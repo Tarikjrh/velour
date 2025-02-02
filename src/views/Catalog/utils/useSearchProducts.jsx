0fe8ad9461f6a7f3330cf50b96845ccb64206236
Tarikjrh <tarik.jarrah@hotmail.com>
2025-02-03 00:12:51 +0200

    refactor: integrate Fuse.js for enhanced product search functionality

--- src/views/Catalog/utils/useSearchProducts.jsx
+++ src/views/Catalog/utils/useSearchProducts.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
-import productsData from "../../../productsData";
+import productsData from "../../../productsData/productsData";
+import Fuse from "fuse.js";
 
 // ----------------------------------------------------------------------
 
@@ -51,13 +52,21 @@ export function useSearchProducts(query) {
   const [isLoading, setIsLoading] = useState(false);
   const [error] = useState(null); // No error simulation
 
+  // Fuse.js configuration
+  const fuse = useMemo(() => {
+    return new Fuse(productsData, {
+      keys: ["name", "description", "tags", "category"], // Search by name and description
+      threshold: 0.3, // Adjust for search sensitivity (lower is stricter)
+      includeScore: true,
+    });
+  }, [productsData]);
+
   useEffect(() => {
     if (query) {
       setIsLoading(true);
       const timeout = setTimeout(() => {
-        const results = productsData.filter((item) =>
-          item.name.toLowerCase().includes(query.toLowerCase())
-        );
+        const results = fuse.search(query).map((result) => result.item); // Extract item from Fuse results
+        console.log("🚀 ~ timeout ~ results:", results);
         setSearchResults(results);
         setIsLoading(false);
       }, 500); // Fake 500ms delay
@@ -66,7 +75,7 @@ export function useSearchProducts(query) {
     } else {
       setSearchResults([]);
     }
-  }, [query]);
+  }, [query, fuse]);
 
   const memoizedValue = useMemo(
     () => ({
