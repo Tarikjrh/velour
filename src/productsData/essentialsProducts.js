3cd7005459bd6ae7c12890887a55e28e1fc99702
Tarikjrh <tarik.jarrah@hotmail.com>
2025-02-02 22:24:39 +0200

    feat: add essentialsProducts data for improved product offerings

---
+++ src/productsData/essentialsProducts.js
@@ -0,0 +1,112 @@
+const essentialProducts = [
+  {
+    id: "7b5f23e9-d4fc-4a89-8d69-123424",
+    name: "Shorts",
+    category: ["Bottoms"],
+    collection: "Essentials",
+    images: [
+      "/products/essentials/essentials_1.png",
+      "/products/essentials/essentials_2.png",
+    ],
+    options: [
+      { type: "length", variants: [{ name: "Long" }, { name: "Short" }] },
+      {
+        type: "pockets",
+        variants: [{ name: "Pocket" }, { name: "No Pocket" }],
+      },
+    ],
+    tags: ["Lab Coats", "Professional Wear", "Medical Uniform"],
+    description:
+      "Experience superior comfort and professionalism with our lab coats. Designed for essentials professionals, these lab coats are lightweight, breathable, and made with durable fabric to endure long workdays.",
+    coverUrl: "/products/essentials/essentials_1.png",
+    colors: ["#ffffff", "#2196F3", "#00BCD4"],
+  },
+  {
+    id: "7b5f23e9-d4fc-4a89-8d69-1yhtrgf",
+    name: "Pants",
+    category: ["Bottoms"],
+    collection: "Essentials",
+    images: [
+      "/products/essentials/essentials_4.png",
+      "/products/essentials/essentials_3.png",
+      "/products/essentials/essentials_17.png",
+    ],
+    options: [
+      { type: "length", variants: [{ name: "Long" }, { name: "Short" }] },
+      {
+        type: "pockets",
+        variants: [{ name: "Pocket" }, { name: "No Pocket" }],
+      },
+    ],
+    tags: ["Lab Coats", "Professional Wear", "Medical Uniform"],
+    description:
+      "Experience superior comfort and professionalism with our lab coats. Designed for essentials professionals, these lab coats are lightweight, breathable, and made with durable fabric to endure long workdays.",
+    coverUrl: "/products/essentials/essentials_4.png",
+    colors: ["#ffffff", "#2196F3", "#00BCD4"],
+  },
+  {
+    id: "7b5f23e9-d4fc-4a89-8d69-myuthrge",
+    name: "Polo Shirt",
+    category: ["Tops"],
+    collection: "Essentials",
+    images: ["/products/essentials/essentials_13.png"],
+    options: [
+      { type: "length", variants: [{ name: "Long" }, { name: "Short" }] },
+      {
+        type: "pockets",
+        variants: [{ name: "Pocket" }, { name: "No Pocket" }],
+      },
+    ],
+    tags: ["Lab Coats", "Professional Wear", "Medical Uniform"],
+    description:
+      "Experience superior comfort and professionalism with our lab coats. Designed for essentials professionals, these lab coats are lightweight, breathable, and made with durable fabric to endure long workdays.",
+    coverUrl: "/products/essentials/essentials_13.png",
+    colors: ["#ffffff", "#2196F3", "#00BCD4"],
+  },
+  {
+    id: "7b5f23e9-d4fc-4a89-8d69-8u6yh",
+    name: "Polos",
+    category: ["Tops"],
+    collection: "Essentials",
+    images: [
+      "/products/essentials/essentials_9.png",
+      "/products/essentials/essentials_11.png",
+      "/products/essentials/essentials_12.png",
+    ],
+    options: [
+      { type: "length", variants: [{ name: "Long" }, { name: "Short" }] },
+      {
+        type: "pockets",
+        variants: [{ name: "Pocket" }, { name: "No Pocket" }],
+      },
+    ],
+    tags: ["Lab Coats", "Professional Wear", "Medical Uniform"],
+    description:
+      "Experience superior comfort and professionalism with our lab coats. Designed for essentials professionals, these lab coats are lightweight, breathable, and made with durable fabric to endure long workdays.",
+    coverUrl: "/products/essentials/essentials_12.png",
+    colors: ["#ffffff", "#2196F3", "#00BCD4"],
+  },
+  {
+    id: "7b5f23e9-d4fc-4a89-8d69-76ewfdc",
+    name: "Jackets",
+    category: ["Tops"],
+    collection: "Essentials",
+    images: [
+      "/products/essentials/essentials_7.png",
+      "/products/essentials/essentials_8.png",
+    ],
+    options: [
+      { type: "length", variants: [{ name: "Long" }, { name: "Short" }] },
+      {
+        type: "pockets",
+        variants: [{ name: "Pocket" }, { name: "No Pocket" }],
+      },
+    ],
+    tags: ["Lab Coats", "Professional Wear", "Medical Uniform"],
+    description:
+      "Experience superior comfort and professionalism with our lab coats. Designed for essentials professionals, these lab coats are lightweight, breathable, and made with durable fabric to endure long workdays.",
+    coverUrl: "/products/essentials/essentials_8.png",
+    colors: ["#ffffff", "#2196F3", "#00BCD4"],
+  },
+];
+export default essentialProducts;
